05e2d3d6137fcc1632cb201ba30239ab57c3969d
AlanTur84 <moumounisawadogo3084@gmail.com>
2025-02-19 17:43:41 +0000

    this is the real end

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///C:/Users/DELL/Documents/3d_surface_api/instance/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
